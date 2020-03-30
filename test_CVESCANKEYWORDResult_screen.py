4571e167bc0eeb46d41870fbf8e8dd4f3efd64c6
humaxnwsqe <humaxnwsqe@gmail.com>
2020-03-30 13:42:03 +0900

    A number of scanning keyword boradcom result has changed from 74 ti 79.

--- test_CVESCANKEYWORDResult_screen.py
+++ test_CVESCANKEYWORDResult_screen.py
@@ -111,9 +111,9 @@ class TestCVESCANKEYWORDRESULT_screen():
         # 6 | click | css=.smaller:nth-child(2) |  | 
         #time.sleep(3)
         #browser.find_element(By.CSS_SELECTOR, ".smaller:nth-child(2)").click()
-        # 7 | assertText | css=.smaller:nth-child(2) | There are 74 CVE entries that match your search. | 
+        # 7 | assertText | css=.smaller:nth-child(2) | There are 79 CVE entries that match your search. | 
         time.sleep(3)
-        assert browser.find_element(By.CSS_SELECTOR, ".smaller:nth-child(2)").text == "There are 74 CVE entries that match your search."
+        assert browser.find_element(By.CSS_SELECTOR, ".smaller:nth-child(2)").text == "There are 79 CVE entries that match your search."
         #assert False
         # 8 | close |  |  | 
         #browser.close()
